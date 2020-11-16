5e97b7412a6dd616f0201fb09b916b107e1f0a72
rafraf9 <rephael.fischer@gmail.com>
2020-11-16 13:16:26 +0200

    added support for premake

---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "BlueCheese"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug", 
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+startproject("Sandbox")
+
+project "BlueCheese"
+	location "BlueCheese"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BC_PLATFORM_WINDOWS",
+			"BC_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "BC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BC_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"BlueCheese/vendor/spdlog/include",
+		"BlueCheese/src"
+	}
+
+	links
+	{
+		"BlueCheese"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BC_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "BC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BC_DIST"
+		optimize "On"
+
